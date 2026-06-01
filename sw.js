3162d29ef04248c1a0a9427b87d844432441f9fc
Camilo Delvasto <camilodelvasto@gmail.com>
2026-06-01 07:10:23 +0200

    deploy: /

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * this file runs.
  */
 
-const BUILD_VERSION = 'c9878325e115';
+const BUILD_VERSION = '8ad563583927';
 const CACHE_STATIC = `grove-static-${BUILD_VERSION}`;
 const CACHE_PAGES = `grove-pages-${BUILD_VERSION}`;
 const CACHE_BLOBS = `grove-blobs-${BUILD_VERSION}`;
