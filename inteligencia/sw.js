0f62ea7682a7763684b2ce26d2268535227a8f2d
Camilo Delvasto <camilodelvasto@gmail.com>
2026-07-15 08:11:30 +0200

    deploy: /inteligencia

--- inteligencia/sw.js
+++ inteligencia/sw.js
@@ -9,7 +9,7 @@
  * this file runs.
  */
 
-const BUILD_VERSION = '8774976d33e0';
+const BUILD_VERSION = 'a7c6b5d2fdf8';
 const CACHE_STATIC = `grove-static-${BUILD_VERSION}`;
 const CACHE_PAGES = `grove-pages-${BUILD_VERSION}`;
 const CACHE_BLOBS = `grove-blobs-${BUILD_VERSION}`;
